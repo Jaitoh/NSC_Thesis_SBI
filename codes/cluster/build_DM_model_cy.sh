1b3a82c27632797f8dd06a29a8392335f1eee286
Jaitoh <1246844791@qq.com>
2023-03-23 17:30:29 +0100

    compile simulator

--- codes/cluster/build_DM_model_cy.sh
+++ codes/cluster/build_DM_model_cy.sh
@@ -3,7 +3,7 @@
 ### Slurm option lines start with #SBATCH 
 ### Here are the SBATCH parameters that you should always consider: 
 #SBATCH --time=0-00:30:00 ## days-hours:minutes:seconds 
-#SBATCH --mem 4G       ## 3000M ram (hardware ratio is < 4GB/core)  16G
+#SBATCH --mem 2G       ## 3000M ram (hardware ratio is < 4GB/core)  16G
 #SBATCH --ntasks=1        ## Not strictly necessary because default is 1 
 #SBATCH --cpus-per-task=2 ## 32 cores per task
 #SBATCH --output=./cluster/build_DM_model_cy.out ## standard out file 
@@ -16,5 +16,5 @@ source activate sbi
 # build DM_Compute
 echo 'build DM_compute'
 cd ./src/simulator
-chmod +x ./build_DM_compute.sh
-./build_DM_compute.sh
+chmod +x ./DM_compute_build.sh
+./DM_compute_build.sh
