17ceb31c1dd3f631be3d14d04bdad0bfaa4860c4
Jaitoh <1246844791@qq.com>
2023-04-22 14:19:56 +0200

    merge dataset

---
+++ codes/src/dataset/merge_dataset.py
@@ -0,0 +1,39 @@
+import os
+import h5py
+
+# list the files in the data directory using python
+data_dir = '../../data/dataset'
+files = [f for f in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, f))]
+
+data_paths = []
+data_file_idxs = []
+for data_file in files:
+    data_path = f'{data_dir}/{data_file}'
+    data_paths.append(data_path)
+    # sort the data paths according to the file name
+    data_paths.sort(key=lambda x: x.split('_part_')[-1].split('.')[0])
+    data_file_idxs.append(int(data_path.split('_part_')[-1].split('.')[0]))
+
+# create a new h5 file to store the combined dataset
+merged_data_path = f'{data_dir}/dataset_L0_exp_set_0.h5'
+
+with h5py.File(merged_data_path, 'w') as merged_data_file:
+    
+    for data_path, data_file_idx in zip(*(data_paths, data_file_idxs)):
+        print(f'adding {data_path} to {merged_data_path}')
+        data_file = h5py.File(data_path, 'r')
+        # copy the data group from the data file to the merged data file
+        set_group = merged_data_file.create_group(f'set_{data_file_idx}')
+        data_file.copy('data/seqC', set_group)
+        data_file.copy('data/probR', set_group)
+        data_file.copy('data/theta', set_group)
+        data_file.close()
+        merged_data_file.flush()
+
+    print(f'\nmerged file has keys: {list(merged_data_file.keys())}')    
+    print(f"\nin one set_0 it has keys: {list(merged_data_file['set_0'].keys())}")
+    print(f"seqC has a shape of: {merged_data_file['set_0']['seqC'].shape}")
+    print(f"theta has a shape of: {merged_data_file['set_0']['theta'].shape}")
+    print(f"probR has a shape of: {merged_data_file['set_0']['probR'].shape}")
+
+# merged_data_file.close()
--- data/download.sh
+++ data/download.sh
@@ -1,16 +1,18 @@
 # x.pt
 # ./gdrive files download 1mr9Wxx5v9qGgYDrhBbfu09rF6g3WhxYI --destination ./tmp/NSC/data/dataset_for_training
-./gdrive files download 1mr9Wxx5v9qGgYDrhBbfu09rF6g3WhxYI --destination ./NSC_Thesis/data/training_datasets
+./gdrive files download 1mr9Wxx5v9qGgYDrhBbfu09rF6g3WhxYI --destination ./NSC/data/training_datasets
 # theta.pt
 # ./gdrive files download 13Xz0bQkP1sOUCsCsqH7sec9t1bVc5ldV --destination ./tmp/NSC/data/dataset_for_training
-./gdrive files download 13Xz0bQkP1sOUCsCsqH7sec9t1bVc5ldV --destination ./NSC_Thesis/data/training_datasets
+./gdrive files download 13Xz0bQkP1sOUCsCsqH7sec9t1bVc5ldV --destination ./NSC/data/training_datasets
 
 # training_dataset.hdf5
-./gdrive files download 1MiVw4EZvHS9bzRjnS801QZ46VKnHDa43 --destination ./NSC_Thesis/data/training_datasets
+./gdrive files download 1MiVw4EZvHS9bzRjnS801QZ46VKnHDa43 --destination ./NSC/data/training_datasets
 
 # sim_data_Ca_Pb_Ma.h5
-./gdrive files download 1Dln5ebWNmkdPTifHPDfPtvRdhxnyFJtw --destination ./NSC_Thesis/data/datasets
-
+./gdrive files download 1Dln5ebWNmkdPTifHPDfPtvRdhxnyFJtw --destination ./NSC/data/datasets
 # gdown --id 19vIy5VeeRTXJuMgkNDY9MY3C_h7AZVZC
 # gdown --id 1Ksu9w8tui4Up_OD8JssNIXEXaSkDZygs
 # gdown --id 188QshxMlik3JW6Z_GM8lj1erx5xtT2cO
+
+./gdrive files download 1yl_F4oaA3mkU2ZJDQF1MNL_fLMM7h7Om --destination ./tmp/NSC/data/dataset
+./gdrive files download 1rSQbY7QHjC3Rovowy6uMOEKBi4HBXwpy --destination ./tmp/NSC/data/dataset
