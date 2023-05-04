77c23e41b587d2f496129c835f630512a8b107e7
Jaitoh <1246844791@qq.com>
2023-05-04 21:34:16 +0200

    debug

--- codes/src/train/do_train_uzh.sh
+++ codes/src/train/do_train_uzh.sh
@@ -13,14 +13,14 @@
 #SBATCH --cpus-per-task=10
 
 #SBATCH --job-name=train_L0
-#SBATCH --output=./cluster/uzh/train_L0/other_logs/output_b3.out
-#SBATCH --error=./cluster/uzh/train_L0/other_logs/error_b3.err
+#SBATCH --output=./cluster/uzh/train_L0/other_logs/output_b2.out
+#SBATCH --error=./cluster/uzh/train_L0/other_logs/error_b2.err
 
 TRAIN_FILE_NAME=train_L0
 CLUSTER=uzh
 # RUN_ID=exp_b1
-# RUN_ID=exp_b2
-RUN_ID=exp_b3
+RUN_ID=exp_b2
+# RUN_ID=exp_b3
 
 # CONFIG_SIMULATOR_PATH=./src/config/test/test_simulator.yaml
 # CONFIG_DATASET_PATH=./src/config/test/test_dataset.yaml
@@ -28,8 +28,8 @@ RUN_ID=exp_b3
 
 CONFIG_SIMULATOR_PATH=./src/config/simulator/exp_set_0.yaml
 # CONFIG_DATASET_PATH=./src/config/dataset/dataset_setting_0_shuffle_1.yaml
-# CONFIG_DATASET_PATH=./src/config/dataset/dataset_setting_0_summary_0.yaml
-CONFIG_DATASET_PATH=./src/config/dataset/dataset_setting_0_summary_1.yaml
+CONFIG_DATASET_PATH=./src/config/dataset/dataset_setting_0_summary_0.yaml
+# CONFIG_DATASET_PATH=./src/config/dataset/dataset_setting_0_summary_1.yaml
 CONFIG_TRAIN_PATH=./src/config/train/train_setting_0.yaml
 
 if [ "${CLUSTER}" == "uzh" ]; then
