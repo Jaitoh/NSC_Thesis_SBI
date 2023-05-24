cbb89e0163e9b756c7ce51c7b3dcefb462c60524
Jaitoh <1246844791@qq.com>
2023-05-24 16:01:36 +0000

    snn train p2-3dur-test-5

--- codes/src/train/do_train_p2_snn.sh
+++ codes/src/train/do_train_p2_snn.sh
@@ -5,15 +5,15 @@ cd ~/tmp/NSC/codes
 source activate sbi
 
 CLUSTER=snn
-PORT=9906
+PORT=6007
 
-RUN_ID=exp-p2-3dur-test-4
+RUN_ID=exp-p2-3dur-test-5
 TRAIN_FILE_NAME=train_L0
 
 DATA_PATH="../data/dataset/dataset_L0_exp_set_0.h5"
 CONFIG_SIMULATOR_PATH=./src/config/simulator/exp_set_0.yaml
 CONFIG_DATASET_PATH=./src/config/dataset/dataset-p2-test.yaml
-CONFIG_TRAIN_PATH=./src/config/train/train-p2-test-3.yaml
+CONFIG_TRAIN_PATH=./src/config/train/train-p2-test-4.yaml
 # CHECK_POINT_PATH='/home/wehe/tmp/NSC/codes/src/train/logs/train_L0/exp-3dur-a1-1/model/best_model_state_dict_run0.pt'
 
 # PRINT_LOG="./cluster/${CLUSTER}/${TRAIN_FILE_NAME}/output_logs/${RUN_ID}.log"
