fefbde748497e800293bf722a83232329c9404ed
Jaitoh <1246844791@qq.com>
2023-05-30 13:18:32 +0200

    add RNN_Multi_Head, test code

--- codes/src/neural_nets/embedding_nets.py
+++ codes/src/neural_nets/embedding_nets.py
@@ -13,20 +13,39 @@ def kaiming_weight_initialization(named_parameters):
         elif 'bias' in name:
             param.data.fill_(0)
 
-class RNN_avg(nn.Module):
+class GRUUnit(nn.Module):
+    def __init__(self, input_dim, hidden_dim):
+        super(GRUUnit, self).__init__()
+        self.gru = nn.GRU(input_dim, hidden_dim, batch_first=True)
+
+    def forward(self, x):
+        output, _ = self.gru(x)
+        return output
+    
+class ResidualBlock(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers):
+        super(ResidualBlock, self).__init__()
+        self.layers = nn.ModuleList([GRUUnit(input_dim if i==0 else hidden_dim, hidden_dim) for i in range(num_layers)])
+        self.linear = nn.Linear(input_dim, hidden_dim)
+
+    def forward(self, x):
+        residual = self.linear(x)
+        for layer in self.layers:
+            out = layer(x)
+            x = out + residual
+            residual = x
+        return x
+
+class RNN_Multi_Head(nn.Module):
     def __init__(self, DM, S, L):
-        super(Conv1D_RNN, self).__init__()
-        # self.conv1 = nn.Conv1d(in_channels=700, out_channels=350, kernel_size=3, stride=1, padding=1)
-        self.convs1 = nn.ModuleList([nn.Conv1d(in_channels=S, out_channels=256, kernel_size=3, stride=1, padding=1) for _ in range(DM)])
-        self.bn1    = nn.BatchNorm1d(num_features=256)
-        self.pool   = nn.AvgPool1d(kernel_size=8, stride=8, padding=0)
-        self.convs2 = nn.ModuleList([nn.Conv1d(in_channels=32, out_channels=8, kernel_size=3, stride=1, padding=1) for _ in range(DM)])
-        self.bn2    = nn.BatchNorm1d(num_features=8)
-        self.convs3 = nn.ModuleList([nn.Conv1d(in_channels=S, out_channels=1, kernel_size=3, stride=1, padding=1) for _ in range(DM)])
-        self.bn3    = nn.BatchNorm1d(num_features=1)
+        super(RNN_Multi_Head, self).__init__()
+        num_layers = 3
+        self.blocks1 = nn.ModuleList([ResidualBlock(S, 64, num_layers) for _ in range(DM)])
+        self.blocks2 = nn.ModuleList([ResidualBlock(64, 8, num_layers) for _ in range(DM)])
+        self.blocks3 = nn.ModuleList([ResidualBlock(8, 1, num_layers) for _ in range(DM)])
         
-        self.batch_norm = nn.BatchNorm1d(num_features=DM)
-        self.gru = nn.GRU(input_size=DM, hidden_size=8, num_layers=1, batch_first=True)
+        self.block4 = ResidualBlock(DM, 16, num_layers)
+        self.block5 = ResidualBlock(16, 8, num_layers)
         
         self.fc1 = nn.Linear(in_features=L*8, out_features=256)
         self.fc2 = nn.Linear(in_features=256, out_features=128)
@@ -36,62 +55,39 @@ class RNN_avg(nn.Module):
         
         kaiming_weight_initialization(self.named_parameters())
 
-    def _avg_pool(self, output):                # Shape: (B, 256, L)
-        output = output.permute(0, 2, 1)        # Shape: (B, L, 256)
-        output = self.pool(output)              # Shape: (B, L, 32)
-        return output.permute(0, 2, 1)          # Shape: (B, 32, L)
-        
     def forward(self, x):
-        #                                           x shape: (B, DM, S, L) - (B, 3*7, 700, 16)
         B, DM, S, L = x.shape
         outputs = []
         for i in range(DM):
-            gru1 = self.gru1[i]
-            gru2 = self.gru2[i]
-            gru3 = self.gru3[i]
-            # print(x.shape)
+            block1 = self.blocks1[i]
+            block2 = self.blocks2[i]
+            block3 = self.blocks3[i]
+            
             sliced = x[:, i, :, :].squeeze(1)       # Shape: (B, S, L) - (B, 700, 16)
-            sliced = sliced.permute(0, 2, 1)        # Shape: (B, L, S) - (B, 16, 700)
-            # print(sliced.shape, x.shape)
+            sliced = sliced.permute(0, 2, 1)        # Shape: (B, L, S)
             
-            # gru1
-            output = gru1(sliced)                   # Shape: (B, L, 256)
-            # print(output.shape)
+            output = block1(sliced)                 # Shape: (B, L, 64)
+            output = block2(output)                 # Shape: (B, L, 8)
+            output = block3(output)                 # Shape: (B, L, 1)
             
-            # gru2
-            output = gru2(output)                   # Shape: (B, L, 8)
-            # print(output.shape)
-            
-            output = gru3(output)                   # Shape: (B, L, 1)
-            
-            output += output_r                      # Shape: (B, 1, L)
-            output = self.bn3(output)               # Shape: (B, 1, L)
-            output = F.relu(output)
-            # print('conv3', output.shape)
-            
-            # append
             outputs.append(output)
-            # print('outputs', output.shape)
         
-        # Concatenate along the DM dimension
-        x = torch.cat(outputs, dim=1)               # Shape: (B, DM, L) - (B, 3*7, 16)
-        # print(x.shape)
-        x = self.batch_norm(x)                      # Shape: (B, DM, L)
-        x = x.permute(0, 2, 1)                      # Shape: (B, L, DM)
-        # print(x.shape)
+        x = torch.cat(outputs, dim=-1)              # Shape: (B, L, DM)
+        x = self.block4(x)                          # Shape: (B, L, 16)
+        x = self.block5(x)                          # Shape: (B, L, 8)
         
-        x, _ = self.gru(x)                          # Shape: (B, L, 8)
-        # print('gru', x.shape)
         x = x.reshape(x.shape[0], -1)               # Shape: (B, L*8)
-        # print('flatten', x.shape)
         x = self.dropout(F.relu(self.fc1(x)))       # Shape: (B, 256)
         x = self.dropout(F.relu(self.fc2(x)))       # Shape: (B, 128)
         x = self.dropout(F.relu(self.fc3(x)))       # Shape: (B, 64)
-        x = self.fc4(x)                             # Shape: (B, 20)
-        # print(x.shape)
+        x = F.relu(self.fc4(x))                     # Shape: (B, 20)
+        
         return x
     
-    
+# test network with summary
+# net = RNN_avg(700, 16, 16)
+# summary(net, (700, 16, 16))
+
 class Conv1D_RNN(nn.Module):
     def __init__(self, DM, S, L):
         super(Conv1D_RNN, self).__init__()
---
+++ codes/test/test_embedding_net.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('./src')
+from nerual_nets import *
+from torchsummary import summary
+
+# test network with summary
+net = RNN_avg(700, 16, 16)
+summary(net, (700, 16, 16))
