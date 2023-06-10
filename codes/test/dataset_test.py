aa3596d184eaf07928a22803a16077e0468ce2f1
Jaitoh <1246844791@qq.com>
2023-06-10 14:49:34 +0000

    对比生成的p1/p3 dataset

---
+++ codes/test/dataset_test.py
@@ -0,0 +1,9 @@
+"""
+Compare the dataset generated using function 
+ - simulate_for_sbi
+ - simulate_and_save
+"""
+
+import sys
+sys.path.append('./src')
+from dataset.simulate_for_sbi import simulate_for_sbi
