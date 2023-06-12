c43245fa94b903572949b101bceaa50aa4fa5594
Jaitoh <1246844791@qq.com>
2023-06-12 13:33:25 +0200

    build(uzh): build DM on uzh server

--- codes/src/simulator/DM_compute_build.sh
+++ codes/src/simulator/DM_compute_build.sh
@@ -3,7 +3,7 @@ rm DM_compute.c
 rm DM_compute.cpython*.so
 rm DM_compute.html
 python3 DM_compute_setup.py build_ext --inplace
-
+# /home/wehe/data/conda/envs/sbi/bin/python3 DM_compute_setup.py build_ext --inplace
 # rm -rf ./src/simulator/build
 # rm ./src/simulator/DM_compute.c
 # rm ./src/simulator/DM_compute.cpython*.so
