dd0f709395c97c5c99c66eeba79e10fe0bd40122
Jaitoh <1246844791@qq.com>
2023-06-23 04:24:54 +0000

    fix(修缮 dataset):

--- codes/src/train/Dataset-features.py
+++ codes/src/train/Dataset-features.py
@@ -1,12 +1,13 @@
 """ Dataset-features.py
 """
-
+import gc
 import time
 import h5py
 import torch
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 import sys
 
@@ -39,6 +40,9 @@ class Feature_Dataset(Dataset):
                 f[set_names[0]][f"feature_{i}"].shape[-1]
                 for i in concatenate_feature_types
             ]
+            print(f"{len_feature_each_type=}")
+
+            self.len_feature_each_type = len_feature_each_type
             n_features = sum(len_feature_each_type)
             n_theta = f[set_names[0]]["theta"].shape[-1]
             T, C, M = f[set_names[0]]["feature_1"].shape[:-1]
@@ -51,7 +55,9 @@ class Feature_Dataset(Dataset):
             self.theta = torch.empty((n_sets, n_T, n_theta))
 
             print(f"loading {n_sets}sets T{(set_T_part[1]-set_T_part[0])*100:.2f}%")
-            for idx_set, set_name in tqdm(enumerate(set_names), total=n_sets):
+            for idx_set, set_name in tqdm(
+                enumerate(set_names), total=n_sets, miniters=n_sets // 5
+            ):
                 # for each selected set
                 # extract feature of partial T from range_T[0] to range_T[1]
                 # concatenate selected different features along the last dimension
@@ -69,7 +75,7 @@ class Feature_Dataset(Dataset):
                     f[set_name]["theta"][range_T[0] : range_T[1], :]
                 )
         self.x = self.x.view(n_sets, n_T, C, M * n_features)
-        print(f"dataset => {self.x.shape=} {self.theta.shape=}")
+        print(f"dataset {self.total_samples=} => {self.x.shape=} {self.theta.shape=}")
 
         indices = torch.arange(self.total_samples)
         self.set_idxs, self.T_idxs, self.C_idxs = unravel_index(indices, (n_sets, n_T, C))
@@ -82,13 +88,20 @@ class Feature_Dataset(Dataset):
         return self.x[set_idx, T_idx, C_idx], self.theta[set_idx, T_idx]
 
 
-def main():
+def main(concatenate_feature_types=[3]):
     data_path = "/mnt/data/dataset/feature-L0-Eset0-100sets-T500-C100.h5"
-    # load dataset into memory
+    data_path = "/home/ubuntu/tmp/NSC/data/dataset/feature-L0-Eset0-100sets-T500-C100.h5"
     f = h5py.File(data_path, "r")
+    sets = list(f.keys())
+    f.close()
+
+    # if Feature exists, delete it
+    if "Feature" in locals():
+        del Feature
+        print("deleted Feature")
+        gc.collect()
 
     # 90% sets for training, 10% for testing
-    sets = list(f.keys())
     train_set_names = sets[:90]
     test_set_names = sets[90:]
     val_set_names = sets
@@ -96,8 +109,6 @@ def main():
     train_set_T_part = [0, 0.9]
     val_set_T_part = [0.9, 1.0]
     test_set_T_part = [0, 0.9]
-
-    concatenate_feature_types = [1, 3, 4, 5]
     Feature = Feature_Dataset(
         data_path=data_path,
         set_names=train_set_names,
@@ -105,6 +116,53 @@ def main():
         concatenate_feature_types=concatenate_feature_types,
     )
 
+    len_feature_each_type = Feature.len_feature_each_type
+
+    # load one sample
+    plt.figure(figsize=(10, 5))
+    for idx in [0, 10, 101, 111, 45000, 45300, 90300, 4005000]:
+        x, theta = Feature[idx]
+        print(
+            f"{idx=:4} {theta=} {Feature.set_idxs[idx], Feature.T_idxs[idx], Feature.C_idxs[idx]} {x.shape=} {theta.shape=}"
+        )  # {x.shape=} {theta.shape=}
+        plt.plot(x, label=f"{idx=}")
+    # set the legend outside the plot
+    plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left", borderaxespad=0.0)
+    plt.grid(alpha=0.2)
+    plt.title(f"{concatenate_feature_types=}\n{len_feature_each_type=}")
+    plt.xlabel("feature index")
+    plt.ylabel("feature value")
+    # vertical line
+    value_accumulate = 0
+    for i in range(3):
+        for value in len_feature_each_type:
+            color = "k" if value == len_feature_each_type[-1] else "grey"
+            plt.axvline(
+                value_accumulate := value_accumulate + value, color=color, linestyle="--"
+            )
+
+    # set boarder width to 3
+    for axis in ["top", "bottom", "left", "right"]:
+        plt.gca().spines[axis].set_linewidth(3)
+    plt.show()
+
 
 if __name__ == "__main__":
-    main()
+    concatenate_feature_types = [1]
+    main(concatenate_feature_types=concatenate_feature_types)
+    concatenate_feature_types = [2]
+    main(concatenate_feature_types=concatenate_feature_types)
+    concatenate_feature_types = [3]
+    main(concatenate_feature_types=concatenate_feature_types)
+    concatenate_feature_types = [4]
+    main(concatenate_feature_types=concatenate_feature_types)
+    concatenate_feature_types = [5]
+    main(concatenate_feature_types=concatenate_feature_types)
+    concatenate_feature_types = [1, 2]
+    main(concatenate_feature_types=concatenate_feature_types)
+    concatenate_feature_types = [1, 2, 3]
+    main(concatenate_feature_types=concatenate_feature_types)
+    concatenate_feature_types = [1, 2, 3, 4]
+    main(concatenate_feature_types=concatenate_feature_types)
+    concatenate_feature_types = [1, 2, 3, 4, 5]
+    main(concatenate_feature_types=concatenate_feature_types)
