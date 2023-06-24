e6f6f250ab0710d8548c178db3c83b01e922ecf0
Jaitoh <1246844791@qq.com>
2023-06-25 01:28:53 +0200

    chore(snn train 2 expriments - june 25):

---
+++ codes/src/train/do_train_p4-snn-0.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=0
+cd ~/tmp/NSC/codes
+source activate sbi
+
+CLUSTER=snn
+# PORT=9906
+
+RUN_ID=p4-5Fs-1D-mh_gru-mdn
+TRAIN_FILE_NAME=train_L0_p4
+
+# DATA_PATH="../data/dataset/dataset_L0_exp_set_0.h5"
+# DATA_PATH="/home/ubuntu/tmp/NSC/data/dataset/feature-L0-Eset0-100sets-T500-C100.h5"
+DATA_PATH="/home/wehe/tmp/NSC/data/dataset/feature-L0-Eset0-100sets-T500-C100.h5"
+CONFIG_DATASET=dataste-p4-5Fs-1D
+CONFIG_TRAIN=train-p4-mh_gru-mdn
+
+# CHECK_POINT_PATH='/home/wehe/tmp/NSC/codes/src/train/logs/train_L0/exp-3dur-a1-1/model/best_model_state_dict_run0.pt'
+
+CONFIG_SIMULATOR=model-0
+CONFIG_EXP=exp-set-0
+CONFIG_PRIOR=prior-0
+CONFIG_X_O=x_o-0
+
+LOG_DIR="./src/train/logs/${TRAIN_FILE_NAME}/${RUN_ID}"
+PRINT_LOG="${LOG_DIR}/${CLUSTER}-${RUN_ID}.log"
+rm -r ${LOG_DIR}
+mkdir -p ${LOG_DIR}
+
+echo "file name: ${TRAIN_FILE_NAME}"
+echo "log_dir: ${LOG_DIR}"
+echo "print_log: ${PRINT_LOG}"
+echo "data_path: ${DATA_PATH}"
+
+code ${PRINT_LOG}
+
+python3 -u ./src/train/${TRAIN_FILE_NAME}.py \
+    hydra.run.dir=${LOG_DIR} \
+    experiment_settings=${CONFIG_EXP} \
+    prior=${CONFIG_PRIOR} \
+    x_o=${CONFIG_X_O} \
+    simulator=${CONFIG_SIMULATOR} \
+    dataset=${CONFIG_DATASET} \
+    train=${CONFIG_TRAIN} \
+    log_dir=${LOG_DIR} \
+    data_path=${DATA_PATH} \
+    seed=100 \
+    >${PRINT_LOG} 2>&1
+# debug=True\
+# & tensorboard --logdir=${LOG_DIR} --port=${PORT}
+# --continue_from_checkpoint ${CHECK_POINT_PATH} \
+
+echo "finished training"
+
+# check behavior output
+python3 -u ./src/train/check_log/check_log.py \
+    --log_dir ${LOG_DIR} \
+    --exp_name ${RUN_ID} \
+    --num_frames 5 \
+    --duration 1000
+
+code ${LOG_DIR}/posterior-${RUN_ID}.gif
