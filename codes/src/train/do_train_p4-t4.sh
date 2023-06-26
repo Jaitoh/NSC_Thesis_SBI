b440540fb78e9371102e1c833608aaee542d4f2c
Jaitoh <1246844791@qq.com>
2023-06-26 17:21:35 +0000

    chore(p4 ctd0):

--- codes/src/train/do_train_p4-t4.sh
+++ codes/src/train/do_train_p4-t4.sh
@@ -8,7 +8,7 @@ CLUSTER=t4
 # PORT=9906
 
 # RUN_ID=p4-5Fs-1D-gru-mdn-ctd0
-RUN_ID=p4-5Fs-1D-gru3-mdn
+RUN_ID=p4-5Fs-1D-gru3-mdn-ctd0
 
 TRAIN_FILE_NAME=train_L0_p4
 
@@ -16,7 +16,7 @@ TRAIN_FILE_NAME=train_L0_p4
 DATA_PATH="/home/ubuntu/tmp/NSC/data/dataset/feature-L0-Eset0-100sets-T500-C100.h5"
 # CONFIG_DATASET=dataste-p4-5Fs-1D-ctd0
 # CONFIG_TRAIN=train-p4-gru-mdn
-CONFIG_DATASET=dataste-p4-5Fs-1D
+CONFIG_DATASET=dataste-p4-5Fs-1D-ctd0
 CONFIG_TRAIN=train-p4-gru3-mdn
 
 # CHECK_POINT_PATH='/home/wehe/tmp/NSC/codes/src/train/logs/train_L0/exp-3dur-a1-1/model/best_model_state_dict_run0.pt'
