df4cf3f748d6fdda63f1ca678d9dcfd46c5bc6ee
Jaitoh <1246844791@qq.com>
2023-07-05 21:59:20 +0000

    chore(p4 distance computation):

--- codes/src/eval/distance.sh
+++ codes/src/eval/distance.sh
@@ -1,1 +1,118 @@
-nice python3 ./src/eval/distance_p4.py > ./src/eval/distance_p4.log 2>&1 &
+PYTHONPATH="home/ubuntu/tmp/NSC/codes:${PYTHONPATH}"
+pwd
+
+TRAIN_ID="train_L0_p4"
+EXP_IDS=(
+    "p4-4Fs-1D-cnn"
+    "p4-3Fs-1D-cnn"
+    "p4-4Fs-1D-cnn"
+    "p4-4Fs-1D-cnn2-0"
+    "p4-4Fs-1D-cnn2-size2"
+    "p4-5Fs-1D-cnn"
+    "p4-5Fs-2D-mh_cnn"
+)
+
+ROOT_DIR="/home/ubuntu/tmp/NSC/codes"
+LOG_DIR="${ROOT_DIR}/src/train/logs/" #${TRAIN_ID}/${EXP_ID}
+
+for EXP_ID in "${EXP_IDS[@]}"; do
+
+    OUT_DIR="${ROOT_DIR}/src/eval/${TRAIN_ID}/${EXP_ID}"
+    mkdir -p ${OUT_DIR}
+
+    nice python3 ./src/eval/distance_p4.py \
+        hydra.run.dir=${ROOT_DIR} \
+        OUT_DIR=${OUT_DIR} \
+        LOG_DIR=${LOG_DIR} \
+        EXP_ID="${TRAIN_ID}/${EXP_ID}" \
+        num_C=100 \
+        num_estimation=3 \
+        >${OUT_DIR}/distance.log 2>&1 &
+    code ${OUT_DIR}/distance.log
+done
+
+#=================================================================================================== p4 t-2
+# TRAIN_ID="train_L0_p4"
+# EXP_IDS=(
+#     "p4-3Fs-1D-mlp"
+#     "p4-5Fs-1D-mlp"
+#     "p4-F2-1D-cnn"
+#     "p4-F3-1D-mlp"
+#     "p4-F5-1D-cnn"
+#     "p4-4Fs-1D-cnn2-size1"
+#     "p4-F1-1D-cnn"
+#     "p4-F2-1D-mlp"
+#     "p4-F4-1D-cnn"
+#     "p4-F5-1D-mlp"
+#     "p4-4Fs-1D-mlp"
+#     "p4-F1-1D-mlp"
+#     "p4-F3-1D-cnn"
+#     "p4-F4-1D-mlp"
+# )
+
+# ROOT_DIR="/home/ubuntu/tmp/NSC/codes"
+# LOG_DIR="${ROOT_DIR}/src/train/logs/" #${TRAIN_ID}/${EXP_ID}
+
+# for EXP_ID in "${EXP_IDS[@]}"; do
+# OUT_DIR="${ROOT_DIR}/src/eval/${TRAIN_ID}/${EXP_ID}"
+
+# mkdir -p ${OUT_DIR}
+#     nice python3 ./src/eval/distance_p4.py \
+#         hydra.run.dir=${ROOT_DIR} \
+#         OUT_DIR=${OUT_DIR} \
+#         LOG_DIR=${LOG_DIR} \
+#         EXP_ID="${TRAIN_ID}/${EXP_ID}" \
+#         num_C=100 \
+#         num_estimation=3 \
+#         >${OUT_DIR}/distance.log 2>&1 &
+#     code ${OUT_DIR}/distance.log
+# done
+
+#
+# TRAIN_ID="train_L0_p5"
+# EXP_IDS=(
+#     "p5-conv_transformer"
+#     "p5-gru3"
+# )
+
+# ROOT_DIR="/home/ubuntu/tmp/NSC/codes"
+# LOG_DIR="${ROOT_DIR}/src/train/logs/" #${TRAIN_ID}/${EXP_ID}
+
+# for EXP_ID in "${EXP_IDS[@]}"; do
+# OUT_DIR="${ROOT_DIR}/src/eval/${TRAIN_ID}/${EXP_ID}"
+
+# mkdir -p ${OUT_DIR}
+#     nice python3 ./src/eval/distance_p4.py \
+#         hydra.run.dir=${ROOT_DIR} \
+#         OUT_DIR=${OUT_DIR} \
+#         LOG_DIR=${LOG_DIR} \
+#         EXP_ID="${TRAIN_ID}/${EXP_ID}" \
+#         num_C=100 \
+#         num_estimation=3 \
+#         >${OUT_DIR}/distance.log 2>&1 &
+#     code ${OUT_DIR}/distance.log
+# done
+
+# #
+# TRAIN_ID="train_L0_p5"
+# EXP_IDS=(
+#     "p5-conv_lstm"
+# )
+
+# ROOT_DIR="/home/ubuntu/tmp/NSC/codes"
+# LOG_DIR="${ROOT_DIR}/src/train/logs/" #${TRAIN_ID}/${EXP_ID}
+
+# for EXP_ID in "${EXP_IDS[@]}"; do
+# OUT_DIR="${ROOT_DIR}/src/eval/${TRAIN_ID}/${EXP_ID}"
+
+# mkdir -p ${OUT_DIR}
+#     nice python3 ./src/eval/distance_p4.py \
+#         hydra.run.dir=${ROOT_DIR} \
+#         OUT_DIR=${OUT_DIR} \
+#         LOG_DIR=${LOG_DIR} \
+#         EXP_ID="${TRAIN_ID}/${EXP_ID}" \
+#         num_C=100 \
+#         num_estimation=3 \
+#         >${OUT_DIR}/distance.log 2>&1 &
+# code ${OUT_DIR}/distance.log
+# done
--- codes/src/eval/distance_p4.py
+++ codes/src/eval/distance_p4.py
@@ -12,6 +12,9 @@ import pickle
 from sklearn.manifold import TSNE
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import hydra
 
 sys.path.append("./src")
 sys.path.append("../../src")
@@ -19,57 +22,69 @@ sys.path.append("../../src")
 from train.train_L0_p4 import Solver
 from features.features import Feature_Generator
 from simulator.model_sim_pR import DM_sim_for_seqCs_parallel_with_smaller_output
+from utils.set_seed import setup_seed
 
+setup_seed(0)
+
+plt.rcParams["font.size"] = 32
+plt.rcParams["axes.titleweight"] = "bold"
+plt.rcParams["axes.labelweight"] = "bold"
+plt.rcParams["axes.linewidth"] = 3
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 PID = os.getpid()
 print(f"PID: {PID}")
 
-# compute the distance between the predicted and the ground truth
-LOG_DIR = "/home/ubuntu/tmp/NSC/codes/src/train/logs"
-EXP_ID = "train_L0_p4/p4-4Fs-1D-cnn"
-DATA_PATH = "/home/ubuntu/tmp/NSC/data/dataset/dataset-L0-Eset0-100sets-T500.h5"
+
 # === compute one example distance ===
 # load config
-config_path = Path(LOG_DIR) / EXP_ID / "config.yaml"
-model_path = Path(LOG_DIR) / EXP_ID / "model" / "best_model.pt"
-config = OmegaConf.load(config_path)
-config.log_dir = str(Path(LOG_DIR) / EXP_ID)
+def load_config(LOG_DIR, EXP_ID):
+    config_path = Path(LOG_DIR) / EXP_ID / "config.yaml"
+    print(f"==>> config_path: {config_path}")
+    config = OmegaConf.load(config_path)
+    config.log_dir = str(Path(LOG_DIR) / EXP_ID)
+
+    model_path = Path(LOG_DIR) / EXP_ID / "model" / "best_model.pt"
+    # check if model exists
+    if not model_path.exists():
+        model_path = Path(LOG_DIR) / EXP_ID / "model" / "model_check_point.pt"
+
+    return model_path, config
+
 
 # get the trained posterior
-solver = Solver(config)
-solver.init_inference().prepare_dataset_network(config, model_path, device=device)
-
-posterior = solver.inference.build_posterior(solver.inference._neural_net)
-solver.inference._model_bank = []
+def get_posterior(model_path, config, device):
+    solver = Solver(config)
+    solver.init_inference().prepare_dataset_network(config, model_path, device=device)
+    posterior = solver.inference.build_posterior(solver.inference._neural_net)
+    solver.inference._model_bank = []
+    return solver, posterior
 
 
 # load seqC, theta, probR
-train_set = solver.inference.train_set_names[0]
-# valid_set = solver.inference.valid_set_names[0] # TODO:
+def load_data(DATA_PATH, solver):
+    # train_set = solver.inference.train_set_names[0]
+    # valid_set = solver.inference.valid_set_names[0]  # TODO:
+    data_set = solver.inference.valid_set_names[0]  # TODO:
 
-# find the prior range limits
-prior_limits = solver._get_limits()
+    # find the prior range limits
+    prior_limits = solver._get_limits()
 
-# with h5py.File(DATA_PATH, "r") as dataset_file:
-dataset_file = h5py.File(DATA_PATH, "r")
-theta_idx = 0
-seqC = torch.from_numpy(dataset_file[train_set]["seqC"][:])
-D, M, S = seqC.shape[0], seqC.shape[1], seqC.shape[2]
-DMS = D * M * S
-theta = torch.from_numpy(dataset_file[train_set]["theta"][theta_idx, :]).view(1, -1)
-probR = torch.from_numpy(dataset_file[train_set]["probR"][..., theta_idx, :])
-print(f"==>> probR.shape: {probR.shape}")
-
-
-num_C = config.dataset.partial_C  # number of Ch samplings
-num_estimation = 3
-chosen_features = config.dataset.concatenate_feature_types
+    # with h5py.File(DATA_PATH, "r") as dataset_file:
+    dataset_file = h5py.File(DATA_PATH, "r")
+    theta_idx = 0
+    seqC = torch.from_numpy(dataset_file[data_set]["seqC"][:])
+    D, M, S = seqC.shape[0], seqC.shape[1], seqC.shape[2]
+    DMS = D * M * S
+    theta = torch.from_numpy(dataset_file[data_set]["theta"][theta_idx, :]).view(1, -1)
+    probR = torch.from_numpy(dataset_file[data_set]["probR"][..., theta_idx, :])
+    print(f"==>> probR.shape: {probR.shape}")
+    return prior_limits, seqC, D, M, S, theta, probR
 
 
-# comput the feature from seqC and chR
-def probR_2_chR(probR, num_C, device):
+def probR_to_chR(probR, num_C, device):
+    "generate chR from sampling probR num_C times"
     if device == "cuda":
         probR = probR.cuda()
         probR = probR.repeat_interleave(num_C, dim=-1)
@@ -80,34 +95,50 @@ def probR_2_chR(probR, num_C, device):
     return chR
 
 
-chR = probR_2_chR(probR, num_C, device)
+class Feature_Gen_Dataset(Dataset):
+    def __init__(self, seqC, chR, D, M, S, chosen_features, FG):
+        self.seqC = seqC
+        self.chR = chR
+        self.D = D
+        self.M = M
+        self.S = S
+        self.chosen_features = chosen_features
+        self.FG = FG
 
-FG = Feature_Generator()
+    def __len__(self):
+        return self.chR.shape[-1]
 
-feature_collection = []
-for i in tqdm(range(num_C)):
-    chR_ = chR[..., i]
-    feature = (
-        FG.compute_kernels(seqC, chR_, D, M, S)
-        .get_provided_feature(chosen_features)
-        .view(1, -1, 1)
-    )
-    feature_collection.append(feature)
-feature_collection = torch.cat(feature_collection, dim=0)
+    def __getitem__(self, idx):
+        chR_ = self.chR[..., idx]
+        feature = (
+            self.FG.compute_kernels(self.seqC, chR_, self.D, self.M, self.S)
+            .get_provided_feature(self.chosen_features)
+            .view(1, -1, 1)
+        )
+        return feature
 
-# pickle feature_collection
-with open(
-    "/home/ubuntu/tmp/NSC/codes/src/eval/features/feature_collection.pkl", "wb"
-) as f:
-    pickle.dump(feature_collection, f)
-print(f"==>> feature_collection.pkl saved\n")
 
-feature_estimated_collection_different_theta = []
+# comput the feature from seqC and chR
+def compute_feature_from_seqC_chR(seqC, D, M, S, chR, chosen_features):
+    FG = Feature_Generator()
+    dataset = Feature_Gen_Dataset(seqC, chR, D, M, S, chosen_features, FG)
+    loader = DataLoader(dataset, num_workers=8, batch_size=1)
+    return torch.cat(list(tqdm(loader)), dim=0)
 
-for j in range(num_estimation):
-    # sample from the posterior with x
-    feature = feature_collection[i]
-    samples = (
+
+def estimate_theta_values(prior_limits, samples):
+    theta_estimated = []
+    for i in tqdm(range(len(prior_limits))):
+        kde = gaussian_kde(samples[:, i])
+        prior_range = np.linspace(prior_limits[i][0], prior_limits[i][1], 2500)
+        densities = kde.evaluate(prior_range)
+        theta_value = prior_range[np.argmax(densities)]
+        theta_estimated.append(theta_value)
+    return theta_estimated
+
+
+def sampling_from_posterior(device, posterior, feature):
+    return (
         posterior.sample(
             (20000,),
             x=feature.cuda() if device == "cuda" else feature,
@@ -117,140 +148,217 @@ for j in range(num_estimation):
         .numpy()
     )
 
-    # find estimated theta using "kde"
-    theta_estimated = []
-    for i in tqdm(range(len(prior_limits))):
-        kde = gaussian_kde(samples[:, i])
-        prior_range = np.linspace(prior_limits[i][0], prior_limits[i][1], 5000)
-        densities = kde.evaluate(prior_range)
-        theta_value = prior_range[np.argmax(densities)]
-        theta_estimated.append(theta_value)
 
-    # print theta with 2 decimal places
-    theta_estimated = np.array(theta_estimated).reshape(1, -1)
-    print(f"==>> theta: {theta}")
-    print(f"==>> theta_estimated: {np.round(theta_estimated, 2)}")
+# LOG_DIR = "/home/ubuntu/tmp/NSC/codes/src/train/logs"
+# EXP_ID = "train_L0_p4/p4-4Fs-1D-cnn"
+# DATA_PATH = "/home/ubuntu/tmp/NSC/data/dataset/dataset-L0-Eset0-100sets-T500.h5"
+# num_C = 100
+# # num_C = config.dataset.partial_C  # number of Ch samplings
+# num_estimation = 3
 
-    # compute the probR based on the estimated theta
-    _, probR_estimated = DM_sim_for_seqCs_parallel_with_smaller_output(
-        seqCs=seqC,
-        prior=theta_estimated,
-        num_prior_sample=1,
-        privided_prior=True,
-        model_name="B-G-L0S-O-N-",
-    )
-    probR_estimated = torch.from_numpy(probR_estimated.reshape(D, M, S, 1))
+
+@hydra.main(config_path="./", config_name="config", version_base=None)
+def main(_config):
+    LOG_DIR = _config.LOG_DIR
+    OUT_DIR = _config.OUT_DIR
+    EXP_ID = _config.EXP_ID
+    DATA_PATH = _config.DATA_PATH
+    num_C = _config.num_C
+    # num_C = config.dataset.partial_C  # number of Ch samplings
+    num_estimation = _config.num_estimation
+    # create features and fig folders
+    os.makedirs(f"{OUT_DIR}/features", exist_ok=True)
+    os.makedirs(f"{OUT_DIR}/fig", exist_ok=True)
+
+    model_path, config = load_config(LOG_DIR, EXP_ID)
+    chosen_features = config.dataset.concatenate_feature_types
+
+    # get the trained posterior
+    solver, posterior = get_posterior(model_path, config, device)
+
+    # load seqC, theta, probR
+    prior_limits, seqC, D, M, S, theta, probR = load_data(DATA_PATH, solver)
+
+    # compute features_true
+    chR = probR_to_chR(probR, num_C, device)
+    features_true = compute_feature_from_seqC_chR(seqC, D, M, S, chR, chosen_features)
+    print(f"==>> features_true.shape: {features_true.shape}")
+
+    # pickle features_true
+    with open(f"{OUT_DIR}/features/features_true.pkl", "wb") as f:
+        pickle.dump(features_true, f)
+    print(f"==>> features_true.pkl saved\n")
 
     feature_estimated_collection = []
-    # compute the feature from seqC and chR_estimated
-    chR_estimated = probR_2_chR(probR_estimated, num_C, device)
-    for i in tqdm(range(num_C)):
-        chR_estimated_ = chR_estimated[..., i]
-        FG = Feature_Generator()
-        feature_estimated = (
-            FG.compute_kernels(seqC, chR_estimated_, D, M, S)
-            .get_provided_feature(chosen_features)
-            .view(1, -1, 1)
-        )
-        # print(f"==>> feature_estimated.shape: {feature_estimated.shape}")
+    theta_estimated_collection = []
+    for i in range(num_estimation):
+        # sample from the posterior with x
+        feature = features_true[i]
+        samples = sampling_from_posterior(device, posterior, feature)
+        # find estimated theta using "kde"
+        theta_estimated = estimate_theta_values(prior_limits, samples)
 
-        feature_estimated_collection.append(feature_estimated)
-    feature_estimated_collection = torch.cat(feature_estimated_collection, dim=0)
-    print(
-        f"==>> feature_estimated_collection.shape: {feature_estimated_collection.shape}"
-    )
-    feature_estimated_collection_different_theta.append(feature_estimated_collection)
+        # print theta with 2 decimal places
+        theta_estimated = np.array(theta_estimated).reshape(1, -1)
+        print(f"==>> theta: {theta}")
+        print(f"==>> theta_estimated: {np.round(theta_estimated, 2)}")
+        theta_estimated_collection.append(np.round(theta_estimated, 2))
+
+        # compute the probR based on the estimated theta
+        _, probR_estimated = DM_sim_for_seqCs_parallel_with_smaller_output(
+            seqCs=seqC,
+            prior=theta_estimated,
+            num_prior_sample=1,
+            privided_prior=True,
+            model_name="B-G-L0S-O-N-",
+        )
+        probR_estimated = torch.from_numpy(probR_estimated.reshape(D, M, S, 1))
+        chR_estimated = probR_to_chR(probR_estimated, num_C, device)
+
+        # compute the feature from seqC and chR_estimated
+        features_estimated = compute_feature_from_seqC_chR(
+            seqC, D, M, S, chR_estimated, chosen_features
+        )
+        print(f"==>> features_estimated.shape: {features_estimated.shape}")
+        feature_estimated_collection.append(features_estimated)
 
     # pickle feature_estimated_collection
     with open(
-        f"/home/ubuntu/tmp/NSC/codes/src/eval/features/feature_estimated_collection_{j}.pkl",
+        f"{OUT_DIR}/features/feature_estimated_collection.pkl",
         "wb",
     ) as f:
         pickle.dump(feature_estimated_collection, f)
-    print(f"==>> feature_estimated_collection_{j}.pkl saved")
+    print(f"==>> feature_estimated_collection.pkl saved")
 
-fig, axes = analysis.pairplot(
-    samples.cpu().numpy(),
-    limits=prior_limits,
-    # ticks=[[], []],
-    figsize=(10, 10),
-    points=theta.cpu().numpy(),
-    points_offdiag={"markersize": 5, "markeredgewidth": 1},
-    points_colors="r",
-    labels=[],
-    upper=["kde"],
-    diag=["kde"],
-)
+    for i in range(num_estimation):
+        # embedding to 2D
+        tsne = TSNE(n_components=2, random_state=0, perplexity=5)
+        reduced_features_true = tsne.fit_transform(features_true.squeeze().numpy())
+        reduced_features_estimated = tsne.fit_transform(
+            feature_estimated_collection[i].squeeze().numpy()
+        )
 
-# load feature_collection
-with open(
-    "/home/ubuntu/tmp/NSC/codes/src/eval/features/feature_collection.pkl", "rb"
-) as f:
-    feature_collection = pickle.load(f)
+        # compute distances - inner distances
+        distances_true = distance.pdist(reduced_features_true, "euclidean")
+        distances_estimated = distance.pdist(reduced_features_estimated, "euclidean")
+        distances_true_square = distance.squareform(distances_true)
+        distances_estimated_square = distance.squareform(distances_estimated)
+        distances_inter = distance.cdist(
+            reduced_features_true, reduced_features_estimated, "euclidean"
+        )
 
-estimate_idx = 99
-with open(
-    f"/home/ubuntu/tmp/NSC/codes/src/eval/features/feature_estimated_collection_{estimate_idx}.pkl",
-    "rb",
-) as f:
-    feature_estimated_collection = pickle.load(f)
-plt.figure()
-plt.plot(feature_collection.squeeze()[0, :].numpy())
-plt.plot(feature_collection.squeeze()[1, :].numpy())
-plt.plot(feature_collection.squeeze()[2, :].numpy())
-plt.show()
+        # Get upper triangular part without the diagonal (k=1)
+        upper_part_true = distances_true_square[
+            np.triu_indices(distances_true_square.shape[0], k=1)
+        ]
+        upper_part_estimated = distances_estimated_square[
+            np.triu_indices(distances_estimated_square.shape[0], k=1)
+        ]
 
-plt.figure()
-plt.plot(feature_estimated_collection.squeeze()[0, :].numpy())
-plt.plot(feature_estimated_collection.squeeze()[1, :].numpy())
-plt.plot(feature_estimated_collection.squeeze()[2, :].numpy())
-plt.show()
+        # plot ==============================
+        fig = plt.figure(figsize=(85, 30))
+        # axs = axs.flatten()
+        grid = plt.GridSpec(2, 5, wspace=0.2, hspace=0.5)
+        ax0 = plt.subplot(grid[0:2, 0:2])
+        ax01 = plt.subplot(grid[0, 2])
+        ax02 = plt.subplot(grid[0, 3])
+        ax03 = plt.subplot(grid[0, 4])
+        ax11 = plt.subplot(grid[1, 2])
+        ax12 = plt.subplot(grid[1, 3])
+        ax13 = plt.subplot(grid[1, 4])
 
-print(f"==>> feature_collection.shape: {feature_collection.shape}")
-print(f"==>> feature_estimated_collection.shape: {feature_estimated_collection.shape}")
+        fig_, axes = analysis.pairplot(
+            samples,
+            limits=prior_limits,
+            # ticks=[[], []],
+            figsize=(10, 10),
+            points=theta.cpu().numpy(),
+            points_offdiag={"markersize": 5, "markeredgewidth": 1},
+            points_colors="r",
+            labels=config.prior.prior_labels,
+            upper=["kde"],
+            diag=["kde"],
+        )
+        # save figure
+        fig_.savefig("pairplot.png")
+        fig_ = plt.imread("pairplot.png")
+        ax0.imshow(fig_)
+        title_name = f"Posterior estimation with validation data\nEXP: {EXP_ID}, {i}th choice samples\nTrue theta:{theta}\nestimated theta:{theta_estimated_collection[i]}"
+        ax0.set_title(title_name)
+        os.remove("pairplot.png")
+        del fig_, axes
 
-tsne = TSNE(n_components=2, random_state=0)
-reduced_features = tsne.fit_transform(feature_collection.squeeze().numpy())
-reduced_features_estimated = tsne.fit_transform(
-    feature_estimated_collection.squeeze().numpy()
-)
-# plt.scatter(reduced_features[:, 0], reduced_features[:, 1], s=1)
-plt.scatter(reduced_features_estimated[:, 0], reduced_features_estimated[:, 1], s=1)
+        im = ax01.imshow(distances_true_square)
+        plt.colorbar(im)
+        ax01.set_title("True embedding's\ndistances matrix")
+        ax01.set_xlabel("True embedding idx")
+        ax01.set_ylabel("True embedding idx")
 
-distances = distance.pdist(reduced_features, "euclidean")
-distances_estimated = distance.pdist(reduced_features_estimated, "euclidean")
+        im = ax02.imshow(distances_estimated_square)
+        plt.colorbar(im)
+        ax02.set_title("Estimated embedding's\ndistances matrix")
+        ax02.set_xlabel("Estimated embedding idx")
+        ax02.set_ylabel("Estimated embedding idx")
 
-distances_square = distance.squareform(distances)
-distances_estimated_square = distance.squareform(distances_estimated)
+        im = ax03.imshow(distances_inter)
+        plt.colorbar(im)
+        ax03.set_title("Inter embedding's\ndistances matrix")
+        ax03.set_xlabel("True embedding idx")
+        ax03.set_ylabel("Estimated embedding idx")
 
-plt.figure()
-plt.imshow(distances_square)
-plt.colorbar()
-plt.show()
+        # scatter plot
+        ax11.scatter(
+            reduced_features_true[:, 0],
+            reduced_features_true[:, 1],
+            s=20,
+            color="b",
+            label="True embedding",
+        )
+        ax11.scatter(
+            reduced_features_estimated[:, 0],
+            reduced_features_estimated[:, 1],
+            s=20,
+            color="r",
+            label="Estimated embedding",
+        )
+        ax11.legend()
+        ax11.set_title("t-SNE 100 feature embedding")
 
-plt.figure()
-plt.imshow(distances_estimated_square)
-plt.colorbar()
-plt.show()
+        # Plot histogram
+        ax12.hist(
+            upper_part_true,
+            bins=30,
+            alpha=0.5,
+            label="True embedding's distances",
+        )
+        ax12.hist(
+            upper_part_estimated,
+            bins=30,
+            alpha=0.5,
+            label="Estimated embedding's distances",
+        )
+        ax12.set_title("Histogram of distances")
+        ax12.set_xlabel("Distance")
+        ax12.set_ylabel("Frequency")
+        ax12.grid(alpha=0.25)
+        ax12.legend()
 
-distances_inter = distance.cdist(
-    reduced_features, reduced_features_estimated, "euclidean"
-)
-# distances_inter_square = distance.squareform(distances_inter)
-plt.figure()
-plt.imshow(distances_inter)
-plt.colorbar()
-plt.show()
+        ax13.plot(features_true.squeeze()[0, :].numpy(), label="True")
+        # ax13.plot(features_true.squeeze()[1, :].numpy())
+        ax13.plot(
+            feature_estimated_collection[i].squeeze()[0, :].numpy(), label="Estimated"
+        )
+        # ax12.plot(feature_estimated_collection.squeeze()[1, :].numpy())
+        ax13.set_title("Example of extracted feature")
+        ax13.set_xlabel("feature index")
+        ax13.set_ylabel("feature value")
+        ax13.grid(alpha=0.25)
+        ax13.legend()
+
+        fig.tight_layout()
+        fig_name = f"{OUT_DIR}/fig/estimation_evaluation_{i}.png"
+        fig.savefig(fig_name)
 
 
-# Get upper triangular part without the diagonal (k=1)
-upper_part = distances_square[np.triu_indices(distances_square.shape[0], k=1)]
-
-# Plot histogram
-plt.figure(figsize=(10, 7))
-plt.hist(upper_part, bins=30, alpha=0.5, color="b", edgecolor="black")
-plt.title("Histogram of distances")
-plt.xlabel("Distance")
-plt.ylabel("Frequency")
-plt.grid(True)
-plt.show()
+if __name__ == "__main__":
+    main()
