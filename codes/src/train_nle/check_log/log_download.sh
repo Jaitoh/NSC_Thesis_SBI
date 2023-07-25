a1f980f0ececbfa691a0366266ea2e0c61e785a9
Jaitoh <1246844791@qq.com>
2023-07-25 21:05:06 +0200

    chore(modify download log):

--- codes/src/train_nle/check_log/log_download.sh
+++ codes/src/train_nle/check_log/log_download.sh
@@ -3,14 +3,19 @@ TRAIN_ID=L0-nle-cnn
 EXP_ID=L0-nle-cnn-dur3-online-copy
 FILE_ID=1vukn9tNDBJPHEBakCcHr2i-NYTJVaftA
 
-# LOG_DIR="/home/wehe/tmp/NSC/codes/src/train_nle/logs"
-LOG_DIR="/home/ubuntu/tmp/NSC/codes/src/train_nle/logs"
+LOG_DIR="/home/wehe/tmp/NSC/codes/src/train_nle/logs"
+# LOG_DIR="/home/ubuntu/tmp/NSC/codes/src/train_nle/logs"
 
-./gdrive files download ${FILE_ID} \
-    --destination ${LOG_DIR}/${TRAIN_ID}
+# cd ~
+# ./gdrive files download ${FILE_ID} \
+#     --destination ${LOG_DIR}/${TRAIN_ID}
 
 cd ${LOG_DIR}/${TRAIN_ID}
 tar -xzf ${EXP_ID}.tar.gz
-mv ${LOG_DIR}/${TRAIN_ID}/${EXP_ID} ./${EXP_ID}
+
+# move to current directory
+LOG_DIR="/home/wehe/tmp/NSC/codes/src/train_nle/logs"
+LOG_DIR="/home/ubuntu/tmp/NSC/codes/src/train_nle/logs"
+mv ./${LOG_DIR}/${TRAIN_ID}/${EXP_ID} ./${EXP_ID}
 rm -r ./home
 rm ${EXP_ID}.tar.gz
