2c4f774eb9e983420a91747fa00f741bf0ae3b55
Jaitoh-t4-2 <1246844791@qq.com>
2023-07-27 16:12:43 +0000

    chore(backup online/offline training log):

--- codes/src/train_nle/check_log/log_download.sh
+++ codes/src/train_nle/check_log/log_download.sh
@@ -3,6 +3,14 @@ TRAIN_ID=L0-nle-cnn
 EXP_ID=L0-nle-cnn-dur3-online-copy
 FILE_ID=1vukn9tNDBJPHEBakCcHr2i-NYTJVaftA
 
+TRAIN_ID=L0-nle-cnn
+EXP_ID=L0-nle-cnn-dur3-online
+FILE_ID=1vX1rN9nDP8-hi2kVkXfYHgJOH-RhhI1z
+
+TRAIN_ID=L0-nle-cnn
+EXP_ID=L0-nle-cnn-dur3-offline
+FILE_ID=1X2JJLyS8DEgA7b4mrloWW_J2a2AXVxyp
+
 # LOG_DIR="/home/wehe/tmp/NSC/codes/src/train_nle/logs"
 LOG_DIR="/home/ubuntu/tmp/NSC/codes/src/train_nle/logs"
 
