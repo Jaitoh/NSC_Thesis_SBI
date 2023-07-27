b51fab59565259356c6909baf9891125254a7463
Jaitoh <1246844791@qq.com>
2023-07-28 01:14:47 +0200

    chore(use $HOME):

--- codes/src/train_nle/get_posterior.sh
+++ codes/src/train_nle/get_posterior.sh
@@ -5,7 +5,7 @@ cd ~/tmp/NSC/codes
 source activate sbi
 
 CLUSTER=snn
-ROOT_DIR="/home/wehe/tmp/NSC"
+ROOT_DIR="$HOME/tmp/NSC"
 
 RUN_ID=L0-nle-cnn
 EXP_ID=L0-nle-cnn-dur3-offline
@@ -13,11 +13,12 @@ CONFIG_DATASET=dataset-nle-cnn-dur3-offline
 CONFIG_POST=posterior-dur3-t0 #snn
 CONFIG_POST=posterior-dur3-t1 #snn
 CONFIG_POST=posterior-dur3-t2 #snn
-# CONFIG_POST=posterior-dur3-t3
-# CONFIG_POST=posterior-dur3-v0
-# CONFIG_POST=posterior-dur3-v1
-# CONFIG_POST=posterior-dur3-v2
-# CONFIG_POST=posterior-dur3-s2
+
+CONFIG_POST=posterior-dur3-t3 #snn
+CONFIG_POST=posterior-dur3-v0 #snn
+CONFIG_POST=posterior-dur3-v1 #snn
+# CONFIG_POST=posterior-dur3-v2 #
+# CONFIG_POST=posterior-dur3-s2 #
 
 LOG_DIR="./src/train_nle/logs/${RUN_ID}/${EXP_ID}"
 PRINT_LOG="${LOG_DIR}/posterior-${RUN_ID}-${CONFIG_POST}.log"
@@ -29,7 +30,8 @@ echo "print_log: ${PRINT_LOG}"
 
 code ${PRINT_LOG}
 
-nice python3 -u ${ROOT_DIR}/codes/src/train_nle/get_posterior.py \
+SCRIPT_PATH=${ROOT_DIR}/codes/src/train_nle/get_posterior.py
+nice python3 -u ${SCRIPT_PATH} \
     hydra.run.dir=${LOG_DIR} \
     log_dir=${LOG_DIR} \
     data_path=${ROOT_DIR}/data/dataset-comb \
