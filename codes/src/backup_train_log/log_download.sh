6d9aa8dcbfc08726729fa0942ba63884f38dde93
Jaitoh <1246844791@qq.com>
2023-08-16 13:41:20 +0200

    .

--- codes/src/backup_train_log/log_download.sh
+++ codes/src/backup_train_log/log_download.sh
@@ -26,6 +26,9 @@ FILE_ID=1ONDJsuoljwVMqBZi9i-mb7aVUxKAHgV-
 EXP_ID=p5a-conv_lstm-Tv2
 FILE_ID=1x-uvyoAI3Fm14HumXi4V3D0WBu3oRWcd
 
+EXP_ID=p5a-conv_lstm-corr_conv-tmp
+FILE_ID=1wrHkE_TaZN-1KKLHMyEPem_lGZICFuFq
+
 # ===== train_npe =====
 # log p4-4Fs-1D-cnn.tar.gz
 # Pipeline=train
